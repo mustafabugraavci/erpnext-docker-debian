f51f186fb1e2c7bcb63134b2273000d0a94a0e1e
Mustafa Buğra Avcı <mbugraavci38@gmail.com>
2025-10-15 11:04:29 +0300

    Update railway-entrypoint.sh

--- railway/railway-entrypoint.sh
+++ railway/railway-entrypoint.sh
@@ -1,12 +1,18 @@
 #!/bin/sh
 set -e
+set -x   # optional: prints each command for logs
 
-echo "-> Set ownership of sites folder"
-chown frappe:frappe /home/frappe/bench/sites
+echo ">>> ENTRYPOINT START"
 
+# Set ownership of sites folder
+echo "-> Setting ownership of sites folder"
+chown -R frappe:frappe /home/frappe/bench/sites
+
+# Link built assets
 echo "-> Linking assets"
 su frappe -c "ln -sf /home/frappe/bench/built_sites/assets /home/frappe/bench/sites/assets"
 su frappe -c "ln -sf /home/frappe/bench/built_sites/apps.json /home/frappe/bench/sites/apps.json"
 su frappe -c "ln -sf /home/frappe/bench/built_sites/apps.txt /home/frappe/bench/sites/apps.txt"
 
+# Execute CMD (railway-cmd.sh)
 exec "$@"
