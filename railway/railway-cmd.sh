21e0002e5cdd17cbf7ec8f9e70addc7025e10681
Mustafa Buğra Avcı <mbugraavci38@gmail.com>
2025-10-15 10:56:59 +0300

    Update railway-cmd.sh

--- railway/railway-cmd.sh
+++ railway/railway-cmd.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 set -e
+set -x   # optional: shows all commands in logs
 
 echo "-> Clearing cache"
 su frappe -c "bench execute frappe.cache_manager.clear_global_cache"
@@ -8,8 +9,8 @@ echo "-> Bursting env into config"
 envsubst '$RFP_DOMAIN_NAME' < /home/$systemUser/temp_nginx.conf > /etc/nginx/conf.d/default.conf
 envsubst '$PATH,$HOME,$NVM_DIR,$NODE_VERSION' < /home/$systemUser/temp_supervisor.conf > /home/$systemUser/supervisor.conf
 
-echo "-> Starting nginx"
-nginx
+echo "-> Starting nginx in foreground"
+/usr/sbin/nginx -g 'daemon off;' &   # run in foreground, keep script alive
 
-echo "-> Starting supervisor"
-/usr/bin/supervisord -c /home/$systemUser/supervisor.conf
+echo "-> Starting supervisord in foreground"
+/usr/bin/supervisord -n -c /home/$systemUser/supervisor.conf
