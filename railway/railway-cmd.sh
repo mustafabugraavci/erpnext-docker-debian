c9f5e66c22cd535d6e103e25376b18f3854e5af4
Mustafa Buğra Avcı <mbugraavci38@gmail.com>
2025-10-15 11:04:43 +0300

    Update railway-cmd.sh

--- railway/railway-cmd.sh
+++ railway/railway-cmd.sh
@@ -1,16 +1,22 @@
 #!/bin/sh
 set -e
-set -x   # optional: shows all commands in logs
+set -x   # optional: prints each command for logs
 
+echo ">>> CMD START"
+
+# Clear ERPNext cache
 echo "-> Clearing cache"
 su frappe -c "bench execute frappe.cache_manager.clear_global_cache"
 
+# Inject environment variables into config files
 echo "-> Bursting env into config"
-envsubst '$RFP_DOMAIN_NAME' < /home/$systemUser/temp_nginx.conf > /etc/nginx/conf.d/default.conf
-envsubst '$PATH,$HOME,$NVM_DIR,$NODE_VERSION' < /home/$systemUser/temp_supervisor.conf > /home/$systemUser/supervisor.conf
+envsubst '$RFP_DOMAIN_NAME' < /home/frappe/temp_nginx.conf > /etc/nginx/conf.d/default.conf
+envsubst '$PATH,$HOME,$NVM_DIR,$NODE_VERSION' < /home/frappe/temp_supervisor.conf > /home/frappe/supervisor.conf
 
+# Start nginx in foreground (so container stays alive)
 echo "-> Starting nginx in foreground"
-/usr/sbin/nginx -g 'daemon off;' &   # run in foreground, keep script alive
+nginx -g 'daemon off;' &
 
+# Start supervisord in foreground
 echo "-> Starting supervisord in foreground"
-/usr/bin/supervisord -n -c /home/$systemUser/supervisor.conf
+/usr/bin/supervisord -n -c /home/frappe/supervisor.conf
